6512b915bc0dc3c02491d485cc2078b8e1170dfa
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-01-07 17:30:51 +0100

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+// question 1
+const numberVariable = 123;
+const StringVariable = 'this is a string';
+const booleanVariable = true;
+
+// question 2
+const firstName = 'johann';
+const lastName = 'ranudd';
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+// question 3
+const myFrog = typeof 'frog';
+console.log(`The type of frog is ${myFrog}`);
+// it was confusing what you wanted me to do here
+
+// question 4
+const orderHasShipped = false;
+
+if (orderHasShipped) {
+  console.log('The order shipped');
+} else {
+  console.log('The order did not ship');
+}
+
+// question 5
+for (let i = 7; i <= 13; i++) {
+  console.log(i);
+}
